6722bc28b80f97640970066e25a1eee08600f6e5
Marcel Haerry <haerry@puzzle.ch>
2008-05-05 11:09:36 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# sshd module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "sshd": }
+class sshd {}
